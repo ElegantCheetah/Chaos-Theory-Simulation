dcba5b15f67134d8de0203e0b3bb4e754b4caa61
ElegantCheetah <Zain4utah@gmail.com>
2021-06-09 13:55:35 -0600

    Tinkering with pendulums

--- Chaos Theory Double Pendulum.py
+++ Chaos Theory Double Pendulum.py
@@ -13,14 +13,16 @@ pygame.init() #Initializing pygame intstance
 size = width, height = 500, 500 #Size of window
 speed = [1,1] #Speed of game
 black = 0, 0, 0 #RGB value of black
+white = 255, 255, 255 #RGB value of white
 
 screen = pygame.display.set_mode(size) #Setting a screen size to the instance of pygame
 
-#pendulumP1 = pygame.draw_point
-#pendulumP2 = pygame.draw_point
+pendulumP1 = pygame.draw.circle(screen, white, (1,1), 1) #Attribute 3 is center point
+pendulumP2 = pygame.draw.circle(screen, white, (1,1), 1) #Attribute 4 is radium
 #pendulumP1Rect = pendulumP1.get_rect()
 #pendulumP2Rect = pendulumP2.get_rect()
 
 while 1:
     screen.fill(black)
+    screen.blit(pendulumP1, pendulumP2)
     pygame.display.flip()
